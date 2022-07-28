16932a6be0e48cbf47b9f8959943ff0c038560ae
Koroleva Maria <mkoroleva5@gmail.com>
2022-07-28 17:05:21 +0300

    feat: add greeting

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -1,13 +1,6 @@
 // 1. Clock and calendar
 
 const time = document.querySelector('.time');
-
-function showTime() {
-    time.textContent = new Date().toLocaleTimeString();
-    setTimeout(showTime, showDate, 1000);
-}
-showTime();
-
 const date = document.querySelector('.date');
 const options = {weekday: 'long', month: 'long', day: 'numeric'};
 const currentDate = new Date().toLocaleDateString('en-US', options);
@@ -15,6 +8,51 @@ const currentDate = new Date().toLocaleDateString('en-US', options);
 function showDate() {
     date.textContent = currentDate;
 }
-showDate();
 
+function showTime() {
+    time.textContent = new Date().toLocaleTimeString();
+    showDate();
+    showGreeting();
+    setTimeout(showTime, 1000);
+}
+
+// 2. Greeting
+
+const greeting = document.querySelector('.greeting');
+const name = document.querySelector('.name');
+const hours = new Date().getHours();
+
+function getTimeOfDay() {
+    if (hours >= 0 && hours < 6) {
+        return 'night';
+    } else if (hours >= 6 && hours < 12) {
+        return 'morning';
+    } else if (hours >= 12 && hours < 18) {
+        return 'afternoon';
+    } else if (hours >= 18 && hours <= 23) {
+        return 'night';
+    }
+}
+
+const timeOfDay = getTimeOfDay();
+const greetingText = `Good ${timeOfDay}`;
+
+function showGreeting() {
+    greeting.textContent = greetingText;
+}
+
+function setLocalStorage() {
+    localStorage.setItem('name', name.value);
+}
+
+function getLocalStorage() {
+    if(localStorage.getItem('name')) {
+      name.value = localStorage.getItem('name');
+    }
+}
+
+window.addEventListener('beforeunload', setLocalStorage);
+window.addEventListener('load', getLocalStorage);
+
+showTime();
 
